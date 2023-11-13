b641e5df25423efc20bc36e79ba1883584cd4c1e
Ian Frankel <ian.frankel@gmail.com>
2023-11-13 16:00:16 -0500

    cartoons listed

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,28 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarf_list):
+    roll = ""
+    for index, dwarf in enumerate(dwarf_list, 1):
+        roll = roll + f"{index}. {dwarf}"
+        if index < len(dwarf_list):
+            roll = roll + f"\n"
+    print(roll)
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(element_list):
+    return [element.title() + '!' for element in element_list]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    not_all_short = False
+    for call in calls:
+        not_all_short = not_all_short or len(call) > 4
+    return not_all_short
 
-def find_the_cheese():
-    pass
+def find_the_cheese(ingredients):
+    cheeses = ["cheddar", "gouda", "camembert"]
+
+    has_cheese = None
+
+    for cheese in cheeses:
+        if cheese in ingredients:
+            has_cheese = cheese
+            return cheese
+        
+    return None
